767108101af498028781d8a711f9896f4966f5a1
Golan Trevize <98289178+SegFun01@users.noreply.github.com>
2022-06-29 10:03:32 -0600

    claves

---
+++ prg/claves.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int aleatorio(int m)
+{
+   int a;
+   a=rand()%m;
+   return a;        
+}
+
+int main(int argc, char *argv[])
+{
+  int i,j,k,n,Ok;
+  int cantidad, letras, numeros; 
+  int seleccion[10];
+  if (argc<4)
+  {
+    printf("Uso: claves cantidad, letras, numeros\n");
+  }
+  else
+  {
+   srand(time(NULL)%1000); 
+   
+   cantidad=atoi(*++argv);
+   letras=atoi(*++argv);  
+   numeros=atoi(*++argv);
+
+   for (k=0;k<cantidad;k++)
+   {
+     for (i=0;i<10;i++)
+     {
+       seleccion[i]= -1; 
+     }
+     for (i=0;i<letras;i++)
+     {
+       Ok=0;
+       while (!Ok)
+       {
+         n=aleatorio(25);
+         if (n==0) n++;
+         if (n==4) n++;
+         if (n==8) n++;
+         if (n==14) n++;
+         if (n==20) n++;
+         Ok=1;
+         for (j=0;j<letras;j++)
+         { 
+           if (seleccion[j]==n) 
+           {
+             Ok=0;
+             exit;
+           }
+         }
+       }
+       seleccion[i]=n;
+       printf("%c",n+97);
+     }
+     for (i=0;i<10;i++)
+     {
+       seleccion[i]= -1; 
+     }    
+     for (i=0;i<numeros;i++)
+     {
+       Ok=0;
+       while (!Ok)
+       {
+         n=aleatorio(10);
+         Ok=1;
+         for (j=0;j<numeros;j++)
+         { 
+           if (seleccion[j]==n) 
+           {
+             Ok=0;
+             exit;
+           }
+         }
+       }
+       seleccion[i]=n;
+       printf("%d",n);
+     }
+     printf("\n");
+   }  
+  }
+}
